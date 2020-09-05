ee9132fff331b0fc039f9213120762ad632e95c1
wahyudi <wahyudi.coding@gmail.com>
2020-09-05 12:35:14 +0700

    init

---
+++ web/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'public/home.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
